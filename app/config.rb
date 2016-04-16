b0d88d3a38625548815def7be2c2d14e79af791c
gotxx <conrad0@onet.eu>
2016-04-16 10:51:06 +0200

    gulp scss tasks added

---
+++ app/config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'assets/scss'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = true
+sourcemap = true
+# output_style = :compressed
