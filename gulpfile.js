1d06ebe4ab0d5f94ecf5ea271c6f0f69314a7bc3
gotxx <conrad0@onet.eu>
2016-04-16 10:50:39 +0200

    gulp scss tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	// sass = require('gulp-compass'),
+	compass = require('gulp-compass');
 
-// define tasks here
-gulp.task('default', function() {
-    // run tasks here
-    // set up watch handlers here
-    
+// sass tasks
+/*gulp.task('sass', function() {
+	return gulp.src('app/assets/scss/style.scss')
+		.pipe(sass()) //Converts Sass to CSS with gulp-sass
+		.pipe(gulp.dest('app/assets/css'));
+});*/
+
+//compass compilation task
+gulp.task('compass', function() {
+  return gulp.src('app/assets/scss/**/*.scss')
+    .pipe(compass({
+      config_file: 'app/config.rb',
+      css: 'app/assets/css',
+      sass: 'app/assets/scss'
+    }))
+    .pipe(gulp.dest('app/assets/temp'));
+});
+
+//compass watch task
+gulp.task('watch', function() {
+	gulp.watch('app/assets/scss/**/*.scss', ['compass']); 
 });
