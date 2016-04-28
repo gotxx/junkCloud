ec72ba17c16c4c3a66972eaf7752e64a01f08366
gotxx <conrad0@onet.eu>
2016-04-28 17:38:45 +0200

    tests

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,1 +1,47 @@
 console.log("script 1");
+
+var myApp = {
+	mojaWlasciwosc: 'vojadiv',
+	init: function(){
+		this.mojaMetoda();	
+	},
+	mojaMetoda: function g() {
+		var _mayApp = this;
+		$('.btn').on('click', '.selector', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			_mayApp.innaMetoda();
+		});
+	},
+	jakisObiekt : {
+		wlasciwosc: "123",
+		nastepnaMetoda: function(){
+			myApp.jakisObiekt.mojaMetoda();
+		}
+	}
+};
+
+function Person(){
+	var _name="asdasd";
+
+	function fun1(){ fun2()}
+	function fun2(){
+		console.log('fun2')
+	}
+
+	this.m1 = function(){
+		this.m2();
+	};
+
+	this.m2 = function(){
+		console.log('m2');
+	};
+
+	this.f1 = fun1;
+	this.f2 = fun2;
+
+	this.getName = functon f(){
+		console.log(this);
+		return _name;
+	};
+}
